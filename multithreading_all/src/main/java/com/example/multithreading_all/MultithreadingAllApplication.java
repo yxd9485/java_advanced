713256e7a6f562af39e03d8565f5bab1793827b8
yangxiaodong <yangxiaodong@nucarf.com>
2023-01-03 18:48:13 +0800

    多线程模块

---
+++ multithreading_all/src/main/java/com/example/multithreading_all/MultithreadingAllApplication.java
@@ -0,0 +1,13 @@
+package com.example.multithreading_all;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultithreadingAllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultithreadingAllApplication.class, args);
+	}
+
+}
--- multithreading_all/src/test/java/com/example/multithreading_all/MultithreadingAllApplicationTests.java
+++ multithreading_all/src/test/java/com/example/multithreading_all/MultithreadingAllApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.java_advanced;
+package com.example.multithreading_all;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JavaAdvancedApplicationTests {
+class MultithreadingAllApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
