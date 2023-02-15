be12d510afd20a66d88fc61dcc4c67a03c77b774
yangxiaodong <yangxiaodong@nucarf.com>
2023-02-15 13:40:05 +0800

    rabbit mq 相关

--- multithreading_all/src/main/java/com/example/multithreading_all/MultithreadingAllApplication.java
+++ multithreading_all/src/main/java/com/example/multithreading_all/MultithreadingAllApplication.java
@@ -2,6 +2,7 @@ package com.example.multithreading_all;
 
 
 
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
@@ -15,6 +16,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 @EnableAsync
 @ServletComponentScan
+@NacosPropertySource(dataId = "LEARN_DEV",groupId = "DEFAULT_GROUP",autoRefreshed = true)
 public class MultithreadingAllApplication {
 
 	public static void main(String[] args) {
--- multithreading_all/src/main/java/com/example/multithreading_all/controller/ThreadController.java
+++ multithreading_all/src/main/java/com/example/multithreading_all/controller/ThreadController.java
@@ -1,9 +1,14 @@
 package com.example.multithreading_all.controller;
 
+import com.alibaba.nacos.api.config.annotation.NacosValue;
 import com.example.multithreading_all.common.aop.RedisLockAnnotation;
 import com.example.multithreading_all.common.enums.RedisLockTypeEnum;
 import com.example.multithreading_all.service.ThreadService;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,15 +24,22 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/java")
+@Slf4j
+@Aspect
+@Component
 public class ThreadController  {
 
     @Autowired
     private ThreadService threadService;
 
+    @Value("${spring.redis.password}")
+    public String pass;
+
     @GetMapping("/thread")
-    @RedisLockAnnotation(typeEnum = RedisLockTypeEnum.ONE, lockTime = 3)
+  //  @RedisLockAnnotation(typeEnum = RedisLockTypeEnum.ONE, lockTime = 3)
     public void getThread(@RequestParam("count") Integer count) {
-         threadService.getThread(count);
+        log.info("--------------->nacos {}",pass);
+        threadService.getThread(count);
     }
 
 
